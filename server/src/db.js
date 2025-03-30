168293405a18601dfd04870ad77034cc50d59c74
Atul Ghormare <atulghormare6@gmail.com>
2025-03-30 21:25:46 +0530

    modified database connection with cloud service

--- server/src/db.js
+++ server/src/db.js
@@ -1,11 +1,18 @@
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// });
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 module.exports = pool;
